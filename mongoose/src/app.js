3998dba41fd32eacfe0eeaff03362226137d4d54
k8pai <thek8pai@gmail.com>
2023-05-13 21:26:37 +0530

    updated with sign up and sign in html pages

--- mongoose/src/app.js
+++ mongoose/src/app.js
@@ -8,17 +8,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/users", userRouter);
+app.use('/users', userRouter);
 app.use(express.json());
 
-mongoose.connect('mongodb://0.0.0.0:27017/singer',
-{
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-}
-)
-.then(() => console.log("Success connecting to Data Base!"))
-.catch((err) => console.log(err));
+mongoose
+	.connect('mongodb://0.0.0.0:27017/singer', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('Success connecting to Data Base!'))
+	.catch((err) => console.log(err));
 
 // // insert some data
 // //1. create collection
@@ -34,12 +33,10 @@ mongoose.connect('mongodb://0.0.0.0:27017/singer',
 //     }
 // });
 
-
 app.get('/', (req, res) => {
-    console.log(req.query.name);
-    res.send({name : "Sagar", age : 27});
-})
+	res.send({ name: 'Sagar', age: 27 });
+});
 
-app.listen(3000, () => {
-    console.log("Server Running at 3000");
-})
+app.listen(5005, () => {
+	console.log('Server Running at 3000');
+});
